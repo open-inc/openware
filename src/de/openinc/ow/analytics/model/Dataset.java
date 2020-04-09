5bea3cd5b53b5f75c36fa786642476432b837c23
Martin Stein <mst206ster@googlemail.com>
2020-04-09 17:59:19 +0200

    Updated Analytics / Dataset models

--- src/de/openinc/ow/analytics/model/Dataset.java
+++ src/de/openinc/ow/analytics/model/Dataset.java
@@ -7,63 +7,82 @@ import java.util.List;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 import de.openinc.ow.core.model.data.OpenWareDataItem;
+import de.openinc.ow.core.model.data.OpenWareNumber;
 import de.openinc.ow.core.model.data.OpenWareValue;
+import de.openinc.ow.core.model.data.OpenWareValueDimension;
 
-
-public class Dataset extends AbstractList<Instance>{
+public class Dataset extends AbstractList<Instance> {
 	private List<Instance> data;
-	DescriptiveStatistics stats; 
-	
-	public Dataset(){
+	DescriptiveStatistics stats;
+
+	public Dataset() {
 		this.data = new ArrayList<>();
 		this.stats = new DescriptiveStatistics();
 	}
-	public Dataset(List<Instance> instances){
-		this.data=instances;	
+
+	public Dataset(List<Instance> instances) {
+		this.data = instances;
 	}
-	public Dataset(double[][] instances){
-		for(double[] instance:instances){
+
+	public Dataset(double[][] instances) {
+		for (double[] instance : instances) {
 			this.data.add(new Instance(instance));
 		}
 	}
-	public Dataset(OpenWareDataItem item){
+
+	public Dataset(OpenWareDataItem item) {
 		this.data = new ArrayList<>();
-		for(OpenWareValue val : item.value()){
+		for (OpenWareValue val : item.value()) {
 			data.add(new Instance(val));
 		}
 	}
-	
-	public int size(){
+
+	public int size() {
 		return this.data.size();
 	}
+
 	@Override
 	public Instance get(int index) {
 		return data.get(index);
 	}
-	
+
 	@Override
 	public boolean add(Instance e) {
 		return data.add(e);
 	}
-	
-	public Instance set(int index, Instance value){
+
+	public Instance set(int index, Instance value) {
 		return this.data.set(index, value);
 	}
-	
-	public Instance getDimensionalVector(int dimension){
+
+	public Instance getDimensionalVector(int dimension) {
 		Instance vector = new Instance();
-		for (Instance i:this.data){
+		for (Instance i : this.data) {
 			vector.push(i.value(dimension));
 		}
 		return vector;
 	}
-	
-	public double getDimensionalAverage(int dimension){
-		double sum =0;
-		for (Instance i:this.data){
-			sum+= i.value(dimension);
+
+	public double getDimensionalAverage(int dimension) {
+		double sum = 0;
+		for (Instance i : this.data) {
+			sum += i.value(dimension);
 		}
-		return sum/this.data.size();
+		return sum / this.data.size();
 	}
-	
+
+	public List<OpenWareValue> toValueList() {
+		List<OpenWareValue> vals = new ArrayList<OpenWareValue>();
+		for (Instance i : data) {
+			OpenWareValue val = new OpenWareValue(i.ts);
+			int y = 0;
+			for (double dVal : i.values) {
+				val.addValueDimension(OpenWareValueDimension.createNewDimension("val" + (y++), "", OpenWareNumber.TYPE)
+						.createValueForDimension(dVal));
+			}
+			vals.add(val);
+		}
+		return vals;
+	}
+
 }
--- src/de/openinc/ow/analytics/model/Instance.java
+++ src/de/openinc/ow/analytics/model/Instance.java
@@ -1,14 +1,10 @@
 package de.openinc.ow.analytics.model;
 
-
-
-
-
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import org.apache.commons.math3.ml.clustering.Clusterable;
 
 import de.openinc.ow.core.model.data.OpenWareNumber;
 import de.openinc.ow.core.model.data.OpenWareValue;
@@ -17,105 +13,119 @@ import de.openinc.ow.core.model.data.OpenWareValueDimension;
 /**
  * Created by Martin on 15.07.2015.
  */
-public class Instance{
-    public  String name;
-    public  List<Double> values;
-    public  long ts;
-    
-    public Instance(){
-    	this.ts = new Date().getTime();
-    	this.values= new ArrayList<Double>();
-    }
+public class Instance implements Clusterable {
+	public String name;
+	public List<Double> values;
+	public long ts;
 
-    public Instance(double[] att) {
-    	this.ts = new Date().getTime();
-    	this.values= new ArrayList<Double>();
-    	for(double value:att){
-        	values.add(value);
-        }
-    }
-    
-    public Instance(OpenWareValue val){
-    	this.ts = val.getDate();
-    	this.values = new ArrayList<>();
-    	for(OpenWareValueDimension dim: val){
-    		if(dim instanceof OpenWareNumber){
-    			values.add(((OpenWareNumber)dim).value());
-    		}
-    	}
-    }
-    public Instance(long ts){
-    	this.ts = ts;
-    	this.values= new ArrayList<Double>();
-    }
+	public Instance() {
+		this.ts = new Date().getTime();
+		this.values = new ArrayList<Double>();
+	}
 
-    public Instance(long ts,double[] att) {
-    	this.ts = ts;
-    	this.values= new ArrayList<Double>();
-    	for(double value:att){
-        	values.add(value);
-        }
-    }
-    
-    public String getName(){
-        return name;
-    };
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setStart(long start) {
-        this.ts = start;
-    }
+	public Instance(double[] att) {
+		this.ts = new Date().getTime();
+		this.values = new ArrayList<Double>();
+		for (double value : att) {
+			values.add(value);
+		}
+	}
 
-   
-    public long getTime(){
-        return ts;
-    };
-    
+	public Instance(OpenWareValue val) {
+		this.ts = val.getDate();
+		this.values = new ArrayList<>();
+		for (OpenWareValueDimension dim : val) {
+			if (dim instanceof OpenWareNumber) {
+				values.add(((OpenWareNumber) dim).value());
+			}
+		}
+	}
 
-    /** Default getter, just insert key that stored the value.
-     *
-     * @param key
-     * @return
-     */
-    public Double value(int index) {
-        return values.get(index);
-    }
+	public Instance(long ts) {
+		this.ts = ts;
+		this.values = new ArrayList<Double>();
+	}
 
-    /** Default setter of key-value pair, Just add water.
-     *
-     * @param key
-     * @param value
-     */
-    public void value(int index, Double value) {
-        values.set(index, value);
-    }
-    
-    public List<Double> values(){
-    	return this.values;
-    }
+	public Instance(long ts, double[] att) {
+		this.ts = ts;
+		this.values = new ArrayList<Double>();
+		for (double value : att) {
+			values.add(value);
+		}
+	}
 
-    /** Default deletion function for internal store. Returns the contained value.
-     * Todo: Add exception for key unknown.
-     *
-     * @param key
-     * @return
-     */
-    public double removeValue(int index){
-        double val = values.remove(index);
-        return val;
-    }
-    
-    public void push(double value){
-    	this.values.add(value);
-    }
-    public double pop(){
-    	return this.values.remove(values.size()-1);
-    }
-    public int size(){
-    	return this.values.size();
-    }
+	public String getName() {
+		return name;
+	};
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public void setStart(long start) {
+		this.ts = start;
+	}
+
+	public long getTime() {
+		return ts;
+	};
+
+	/**
+	 * Default getter, just insert key that stored the value.
+	 *
+	 * @param key
+	 * @return
+	 */
+	public Double value(int index) {
+		return values.get(index);
+	}
+
+	/**
+	 * Default setter of key-value pair, Just add water.
+	 *
+	 * @param key
+	 * @param value
+	 */
+	public void value(int index, Double value) {
+		values.set(index, value);
+	}
+
+	public List<Double> values() {
+		return this.values;
+	}
+
+	/**
+	 * Default deletion function for internal store. Returns the contained value.
+	 * Todo: Add exception for key unknown.
+	 *
+	 * @param key
+	 * @return
+	 */
+	public double removeValue(int index) {
+		double val = values.remove(index);
+		return val;
+	}
+
+	public void push(double value) {
+		this.values.add(value);
+	}
+
+	public double pop() {
+		return this.values.remove(values.size() - 1);
+	}
+
+	public int size() {
+		return this.values.size();
+	}
+
+	@Override
+	public double[] getPoint() {
+		double[] res = new double[values.size()];
+		int i = 0;
+		for (double val : values) {
+			res[i++] = val;
+		}
+		return res;
+	}
 
 }

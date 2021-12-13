8312eff6c02a3d03be15a3f770984a4a7201ca44
Martin <martin.stein@uni-siegen.de>
2021-12-13 14:52:30 +0100

    Add OpenWareValueDimension.inferDimension()

--- src/de/openinc/model/data/OpenWareValueDimension.java
+++ src/de/openinc/model/data/OpenWareValueDimension.java
@@ -1,6 +1,7 @@
 package de.openinc.model.data;
 
 import org.apache.commons.text.StringEscapeUtils;
+import org.json.JSONObject;
 
 import de.openinc.ow.helper.DataConversion;
 
@@ -25,6 +26,7 @@ public abstract class OpenWareValueDimension {
 
 	abstract public OpenWareValueDimension cloneDimension();
 
+	
 	public String getName() {
 		return name;
 	}
@@ -41,19 +43,66 @@ public abstract class OpenWareValueDimension {
 		this.unit = unit;
 	}
 
-	public static OpenWareValueDimension createNewDimension(String name, String unit, String odType) {
+	/**
+	 * Use this method to infer a {@link OpenWareValueDimension} based on a given value. <b>Use with care</b> since it, e.g. will try to parse booleans/numbers out of strings, etc. If all parsing fails a {@link OpenWareString} will be returned 
+	 * @param value The value which will be used to infer the valuetype
+	 * @param unit	Optional unit which will be assigned. Can be {@code NULL}
+	 * @param name 	Optional Name which will be assigned. Can be {@code NULL}. If not provided, empty string will be used
+	 * @throws {@link IllegalArgumentException} if the provided value is {@code NULL}
+	 * @return The educated guessed {@link OpenWareValueDimension}
+	 */
+	public static OpenWareValueDimension inferDimension(Object value, String unit, String name) throws IllegalArgumentException{
+		if(value ==null) throw new IllegalArgumentException("Value cannot be null when infering type");
+		if(value instanceof Boolean) {
+			return createNewDimension(name == null ? "":name, unit==null?"":unit, OpenWareBoolValue.TYPE, value);
+		}
+		
+		if(value.toString().toLowerCase().equals("true")||value.toString().toLowerCase().equals("false")) {
+			return createNewDimension(name == null ? "":name, unit==null?"":unit, OpenWareBoolValue.TYPE, Boolean.valueOf(value.toString().toLowerCase()));
+		}
+		
+		if(value instanceof Number) {
+			return createNewDimension(name == null ? "":name, unit==null?"":unit, OpenWareNumber.TYPE, ((Number)value).doubleValue());	
+		}
+		
+		try {
+			double d = Double.parseDouble(value.toString());
+			return createNewDimension(name == null ? "":name, unit==null?"":unit, OpenWareNumber.TYPE, d);			
+		}catch(Exception e) {
+			//NOTHING TO DO
+		}
+		if(value instanceof JSONObject) {
+			try {
+				OpenWareGeo cGeo =new OpenWareGeo(name == null ? "":name, unit==null?"":unit,(JSONObject) value); 
+				if(cGeo.value()!=null) {
+					return cGeo;	
+				}
+				 			
+			}catch(Exception e) {
+				return new OpenWareGeneric(name == null ? "":name, unit==null?"":unit,(JSONObject) value);
+			}	
+		}
+		
+		
+		return new OpenWareString(name == null ? "":name, unit==null?"":unit,value.toString());
+		
+	}
+	public static OpenWareValueDimension createNewDimension(String name, String unit, String odType, Object value) {
 		switch (odType.toLowerCase()) {
 		case "number":
-			return new OpenWareNumber(name, unit, null);
+			return new OpenWareNumber(name, unit, value!=null?(double)value:null);
 		case "string":
-			return new OpenWareString(name, unit, null);
+			return new OpenWareString(name, unit, value!=null?(String)value:null);
 		case "boolean":
-			return new OpenWareBoolValue(name, unit, null);
+			return new OpenWareBoolValue(name, unit, value!=null?(boolean)value:null);
 		case "geo":
-			return new OpenWareGeo(name, unit, null);
+			return new OpenWareGeo(name, unit, value!=null?(JSONObject)value:null);
 		default:
-			return new OpenWareGeneric(name, unit, null);
-		}
+			return new OpenWareGeneric(name, unit, value!=null?(JSONObject)value:null);
+		}	
+	}
+	public static OpenWareValueDimension createNewDimension(String name, String unit, String odType) {
+		return createNewDimension(name, unit, odType, null);
 	}
 
 	@Override

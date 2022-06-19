1554449d7aa43331bd5f699b94184fb214a68eac
Martin <martin.stein@uni-siegen.de>
2022-06-19 18:14:26 +0200

    Log Errors in Filter Transformer

--- src/de/openinc/ow/transformation/FilterTransformer.java
+++ src/de/openinc/ow/transformation/FilterTransformer.java
@@ -1,11 +1,11 @@
 package de.openinc.ow.transformation;
 
-
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.json.JSONObject;
+import org.springframework.expression.EvaluationException;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -14,23 +14,32 @@ import de.openinc.api.TransformationOperation;
 import de.openinc.model.data.OpenWareDataItem;
 import de.openinc.model.data.OpenWareValue;
 import de.openinc.model.user.User;
+import de.openinc.ow.OpenWareInstance;
 
 public class FilterTransformer extends TransformationOperation {
 
 	OpenWareDataItem res;
+
 	@Override
 	public TransformationOperation apply(User user, OpenWareDataItem old, JSONObject params) throws Exception {
 		res = old.cloneItem(false);
 		ExpressionParser parser = new SpelExpressionParser();
 		String expression = params.getString("filterExpression");
 		Expression exp = parser.parseExpression(expression);
-		
-		List<OpenWareValue>filtered = old.value().stream().parallel().filter(new Predicate<OpenWareValue>() {
+
+		List<OpenWareValue> filtered = old.value().stream().parallel().filter(new Predicate<OpenWareValue>() {
 			@Override
 			public boolean test(OpenWareValue t) {
 				Holder holder = new Holder();
 				holder.val = t;
-				return exp.getValue(holder, Boolean.class);
+				try {
+					return exp.getValue(holder, Boolean.class);
+				} catch (EvaluationException e) {
+					OpenWareInstance.getInstance()
+							.logTrace("Filter Error: " + e.getMessage() + "\n" + e.getCause().toString());
+					return false;
+				}
+
 			}
 		}).collect(Collectors.toList());
 		res.value().addAll(filtered);
@@ -42,10 +51,11 @@ public class FilterTransformer extends TransformationOperation {
 	public OpenWareDataItem getResult() {
 		// TODO Auto-generated method stub
 		return res;
-		
+
 	}
 
 }
-class Holder{
+
+class Holder {
 	public OpenWareValue val;
 }
